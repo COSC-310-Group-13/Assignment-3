938079555b60eb0453d4a09a64b56a17cf75b906
iwanlevin <apple.iwan@gmail.com>
2021-03-22 14:43:06 -0700

    chatbot basic unit testing is done

--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -1,14 +1,14 @@
+from pickle import NONE
 import pytest
-import unittest
 from chatbot.chatbot import ChatBot as cb
-import math
 import random
 
 @pytest.fixture
 def chatbot():
     chatbot = cb()
+    chatbot.extractQuotes('quotes.txt')
     return chatbot
-
+    
 @pytest.fixture
 def userHellos():
     userHellos = ['hello', "what's up", 'hey', 'hi', 'hello', 'howdy', 'sup', 'hey there']
@@ -21,17 +21,13 @@ def botHellos():
 
 @pytest.fixture
 def rnd_ArrayInt():
-    rndint = {}
+    rndint = []
     for i in range(0,1000):
-        rnd = random.randfloat(0,100)
-        math.round(rnd)
+        rnd = random.uniform(0,100)
+        rnd = round(rnd,5)
         rndint.append(rnd)
     
     return rndint
-
-def test_getQoutes():
-
-    assert True
 
 def test_helloMessage(chatbot, userHellos, botHellos):
     p = False  
@@ -43,10 +39,22 @@ def test_helloMessage(chatbot, userHellos, botHellos):
 
     assert p
 
-def test_sortIndexList():
+def test_sortIndexList(chatbot, rnd_ArrayInt):
+    sorted_rnd = chatbot.sortIndexList(rnd_ArrayInt)
+    for i in range(0,len(sorted_rnd) - 2):
+        if sorted_rnd[i] > sorted_rnd[i+1]:
+            p = True
+        else:
+            P = False
+    assert p
 
-    assert True
+def test_botResponse(chatbot):
 
-def test_botResponse():
-
-    assert True
+    dr = ["I'm sorry, I didn't quite understand what you just typed.","Sorry I'm not capable talking about that right now.",
+                                  "Your choice of discussion is out of my range.", "I didn't get that could you try again?",
+                                  "Unfortunealy I don't recognize what your trying to tell me."]
+    if chatbot.botResponse("construction workers") in dr:
+        p = False
+    else:
+        p = True
+    assert p
